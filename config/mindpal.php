3d4766d031236c29eab016e236e90e2c473a828e
Young Dev. <iliyaaabdurrazaq@gmail.com>
2025-05-02 18:33:30 +0100

    Update mindpal.php
    Removing the dbname and password

--- config/mindpal.php
+++ config/mindpal.php
@@ -1,8 +1,8 @@
 <?php
-$host = "127.0.0.1:3306"; 
-$dbname = "u873311945_mind"; 
-$username = "u873311945_mind";
-$password = "Mind@fud123";  
+$host = ""; 
+$dbname = ""; 
+$username = "";
+$password = "";  
     $conn = new mysqli($host, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Connection Error". $conn->connect_error);
